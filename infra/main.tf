cb1475f8ef5a92c67b62aaaa462b43ed0fdb0f1d
Fredrik Vedvik <fredrik@vedvik.tech>
2023-09-17 09:08:01 +0200

    vm

--- infra/main.tf
+++ infra/main.tf
@@ -4,8 +4,16 @@ terraform {
   required_providers {
     # For Azure projects
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 3.45"
     }
   }
+}
+
+data "azurerm_resource_group" "main" {
+  name = local.props.azure.resource_group
+}
+
+locals {
+  props = merge(jsondecode(var.props), jsondecode(var.deployment_props))
 }
