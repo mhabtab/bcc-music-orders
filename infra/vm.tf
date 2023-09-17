c3bf2b10f45258240e82c72124b5046f4e59cafd
Fredrik Vedvik <fredrik@vedvik.tech>
2023-09-17 11:06:49 +0200

    443 and 80

--- infra/vm.tf
+++ infra/vm.tf
@@ -5,13 +5,27 @@ resource "azurerm_network_security_group" "main" {
 }
 
 resource "azurerm_network_security_rule" "main" {
-  name                        = "web-traffic"
+  name                        = "web-traffic-80"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "80,443"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
+resource "azurerm_network_security_rule" "main" {
+  name                        = "web-traffic-443"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = data.azurerm_resource_group.main.name
